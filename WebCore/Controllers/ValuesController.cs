ea904e84de0f2a434bda612f2b27efb81c8d025b
zhanggang <zhanggang2019@boco.com.cn>
2019-07-30 18:32:49 +0800

    增加dockerfile ,以及在Centos上运行Docker容器，以及卷挂载用于数据、Log持久化（在Dockfile和run命令中都有）

---
+++ WebCore/Controllers/ValuesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public ValuesController()
+        {
+        }
+
+        [HttpGet,Route("GetValues/{value}")]
+        [Description("测试使用")]
+        public string GetValues(string value)
+        {
+            Logger.Info(value);
+            return value;
+        }
+    }
+}
